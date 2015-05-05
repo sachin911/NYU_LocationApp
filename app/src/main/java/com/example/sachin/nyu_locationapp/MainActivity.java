597dc334f9657e24a88f958bb841a2878ebc1a2e
sachin <sn1628@nyu.edu>
2015-05-04 21:16:10 -0400

    Changes includes rest connection to the web service and address verification

--- app/src/main/java/com/example/sachin/nyu_locationapp/MainActivity.java
+++ app/src/main/java/com/example/sachin/nyu_locationapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sachin.nyu_locationapp;
 
 import android.app.Activity;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,17 +14,27 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationListener;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+import java.util.Locale;
 
 
 public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener {
-
+    private static final String URL = "http://tm.poly.edu:8080/com.nyu.location/rest/address";
     private static final String TAG = MainActivity.class.getSimpleName();
-
+    private  TextView errorMsg = null;
+    //private  ProgressDialog prgDialog;
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
     //time interval between location updates
     private static int UPDATE_INTERVAL = 10000; // 10 sec
@@ -37,6 +49,7 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
     // UI elements
     private TextView lblLocation;
     private Button btnShowLocation, btnStartLocationUpdates;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,10 +71,12 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
 
         // Show location button click listener
         btnShowLocation.setOnClickListener(new View.OnClickListener() {
-
+            RequestParams params = new RequestParams();
             @Override
             public void onClick(View v) {
                 displayLocation();
+                params = getParams();
+                invokeWS(params);
             }
         });
 
@@ -72,6 +87,7 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
             @Override
             public void onClick(View v) {
                 togglePeriodicLocationUpdates();
+
             }
         });
 
@@ -128,18 +144,20 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
     }
 
     /**
-     * Displays the Location in terms of latitude and Longitudes
+     * Displays the Location in terms of address
      * */
     private void displayLocation() {
 
         mLastLocation = LocationServices.FusedLocationApi
                 .getLastLocation(mGoogleApiClient);
-
+        String add;
         if (mLastLocation != null) {
             double latitude = mLastLocation.getLatitude();
             double longitude = mLastLocation.getLongitude();
 
-            lblLocation.setText(latitude + ", " + longitude);
+            //lblLocation.setText(latitude + ", " + longitude);
+             add = getCompleteAddressString(latitude,longitude);
+            lblLocation.setText(add);
 
         } else {
 
@@ -206,11 +224,7 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
         }
     }
 
-//    @Override
-////    protected void onPause() {
-////        super.onPause();
-////        stopLocationUpdates();
-////    }
+
 
     /**
      * Google api callback methods
@@ -250,19 +264,114 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
     }
 
 
-//    @Override
-//    public void onStatusChanged(String provider, int status, Bundle extras) {
-//
-//    }
-//
-//    @Override
-//    public void onProviderEnabled(String provider) {
-//
-//    }
-//
-//    @Override
-//    public void onProviderDisabled(String provider) {
-//
-//    }
+    /*
+    This method obtains the parameters to be sent to rest service
+     */
+    public RequestParams getParams() {
+
+        RequestParams params = new RequestParams();
+        String adr;
+        double latitude = 0;
+        double longitude = 0;
+        mLastLocation = LocationServices.FusedLocationApi
+                .getLastLocation(mGoogleApiClient);
+
+        if (mLastLocation != null) {
+            latitude = mLastLocation.getLatitude();
+            longitude = mLastLocation.getLongitude();
+
+            }
+
+        adr = getCompleteAddressString(latitude,longitude);
+       params.put("address",adr);
+
+        return params;
+    }
+
+    /*
+    This method converts co-ordinates into postal address
+     */
+    private String getCompleteAddressString(double LATITUDE, double LONGITUDE) {
+        String strAdd = "";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(LATITUDE, LONGITUDE, 1);
+            if (addresses != null) {
+                Address returnedAddress = addresses.get(0);
+                StringBuilder strReturnedAddress = new StringBuilder("");
+
+                for (int i = 0; i < returnedAddress.getMaxAddressLineIndex(); i++) {
+                    strReturnedAddress.append(returnedAddress.getAddressLine(i)).append("\n");
+                }
+                strAdd = strReturnedAddress.toString();
+                Log.w("Current add", "" + strReturnedAddress.toString());
+            } else {
+                Log.w("Current add", "No Address returned!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.w("loction address", "Cannot get Address!");
+        }
+        return strAdd;
+    }
+
+    /*
+    This method invokes the web services
+     */
+    public void invokeWS(RequestParams params){
+
+        // Make RESTful webservice call using AsyncHttpClient object
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        //client.get("http://10.0.2.2:8080/com.sachin.nyulocation/rest/locate",params ,new AsyncHttpResponseHandler() {
+        client.get(URL,params ,new AsyncHttpResponseHandler() {
+            // When the response returned by REST has Http response code '200'
+            @Override
+            public void onSuccess(int statusCode,String response) {
+
+                try {
+                    // JSON Object
+                    JSONObject obj = new JSONObject(response);
+                    // When the JSON response has status boolean value assigned with true
+                    if(obj.getBoolean("status")){
+                        Toast.makeText(getApplicationContext(), "You are at the office", Toast.LENGTH_LONG).show();
+
+                    }
+                    // Else display error message
+                    else{
+                        errorMsg.setText(obj.getString("error_msg"));
+                        Toast.makeText(getApplicationContext(), obj.getString("error_msg"), Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    Toast.makeText(getApplicationContext(), "Server's JSON response might be invalid", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+
+                }
+            }
+            // When the response returned by REST has Http response code other than '200'
+            @Override
+            public void onFailure(int statusCode, Throwable error,
+                                  String content) {
+                // Hide Progress Dialog
+                //prgDialog.hide();
+                // When Http response code is '404'
+                if(statusCode == 404){
+                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code is '500'
+                else if(statusCode == 500){
+                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code other than 404, 500
+                else{
+                    //Toast.makeText(getApplicationContext(),statusCode,Toast.LENGTH_LONG).show();
+                    Log.e("omg android", statusCode + " " + error.getMessage());
+                    Toast.makeText(getApplicationContext(), "Device might not be connected to Internet or remote server is not up and running", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
 }
 
